7c1d1f138b15d5cce91799c3c7e8f311af40fcb7
treeswithlove <monet.2017@gmail.com>
2019-04-10 09:38:15 -0400

    actual first commit

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-
-
-
-# gameProjectrepo connected
+# gameProject
+https://boring-brattain-2fc9b7.netlify.com     
+https://trello.com/b/spJyH8lK/project-1-game    
+images/20190409_115637.jpg 
+images/20190409_151712.jpg 
---
+++ app.js
@@ -0,0 +1,20 @@
+//             div
+//             / 
+//            button  
+//          /   |    \
+//  innerText  modal  eventlistener
+//        /      | \     \
+// "$ amount"   /   \   remove div
+//       question   input
+//                     \
+
+//                    if( input == answer){
+//                     user1Points + Number(innerText)
+//                    }else {
+//                     userpoints - Number(innerText)
+//                    }
+
+
+//two player game
+// two scores displayed in innerText
+//
