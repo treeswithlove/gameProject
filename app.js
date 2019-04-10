5c8301225933e190204d44835edca9b8f51cdd19
treeswithlove <monet.2017@gmail.com>
2019-04-10 12:22:04 -0400

    made changes to pseudocode

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
+//create a for loop to create the <divs class="box"> in parent div
+//inside div is a square button
+// if statement inside for loop to number the button's innerText with points
+//create event listener for each box to grab the [i] object
+//[i] object includes question and answer choices
+// create event listner for click to remove innerText
+
+
+
+
+
+
+
+
 //             div
 //             / 
 //            button  
 //          /   |    \
 //  innerText  modal  eventlistener
 //        /      | \     \
-// "$ amount"   /   \   remove div
+// "$ amount"   /   \   remove innerText
 //       question   input
 //                     \
 
