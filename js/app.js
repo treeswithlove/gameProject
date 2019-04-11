5837d99b39f01c75a0867363ee1c5fb92e692972
treeswithlove <monet.2017@gmail.com>
2019-04-10 23:45:38 -0400

    edited folder layout and added comments to htmls

---
+++ js/app.js
@@ -0,0 +1,32 @@
+
+//            (<main>)
+//               |
+//             div(s)
+//             / 
+//            button  
+//          /   |    \
+//  innerText  modal  eventlistener
+//        /      | \     \
+// "$ amount"   /   \   remove innerText
+//       question   input
+//                     \
+
+//                    if( answer == answer){
+//                     userPoints + Number(innerText)
+//                    }else {
+//                     userpoints - Number(innerText)
+//                    }
+
+
+//welcome page with start button
+//create a for loop to create the <divs class="box"> in parent div (<main>)
+//inside div is a square button
+// if statement inside for loop to number the button's innerText with points
+//create event listener for each box to grab the [i] object
+//[i] object includes question and answer choices
+// create event listner for click to remove button
+//  if( guess === answer){
+//                     userPoints + Number(innerText)
+//                    }else {
+//                     userpoints - Number(innerText)
+//                    }
