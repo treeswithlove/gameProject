36b552445111b70901dbe008c01d413d390d1a2e
treeswithlove <monet.2017@gmail.com>
2019-04-11 10:32:17 -0400

    created if statements and appended buttons and removebutton

--- js/app.js
+++ js/app.js
@@ -18,8 +18,35 @@
 //                    }
 
 
-//welcome page with start button
+
 //create a for loop to create the <divs class="box"> in parent div (<main>)
+for(let i = 1; i<31 ;i++){
+    let text
+    if (i < 6){
+        text = "Category " + i;
+    }
+    else if (i < 11){
+        text = "100"
+    }
+    else if (i < 16){
+        text = "200"
+    }
+    else if (i < 21){
+        text = "300"
+    }
+    else if (i < 26){
+        text = "400"
+    }
+    else if (i < 31){
+        text = "500"
+    }
+    $("main").append("<div class='questionBlocks'>"+"<a href='#'>" + "<button class='modalButton'>" + text + "</button>"+"</a>"+"</div>")
+    // $("main").on("click",".modalButton",()=>{
+    //     $(event.target).remove()
+    // })
+  
+}
+
 //inside div is a square button
 // if statement inside for loop to number the button's innerText with points
 //create event listener for each box to grab the [i] object
