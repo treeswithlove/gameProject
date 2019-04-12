a0fda205ab42eba462f1d03a8bc755c078cda337
treeswithlove <monet.2017@gmail.com>
2019-04-11 23:58:46 -0400

    psedocode and comments

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 
-//            (<main>)
+//            √(<main>)
 //               |
-//             div(s)
+//             √div(s)
 //             / 
-//            button  
+//            √button  
 //          /   |    \
-//  innerText  modal  eventlistener
+//  √innerText  modal  √eventlistener
 //        /      | \     \
-// "$ amount"   /   \   remove innerText
+// √userPoints   /   \   √remove innerText
 //       question   input
 //                     \
 //                    if( guess == answer){
@@ -21,12 +21,15 @@
 //create a for loop to create the <divs class="box"> in parent div (<main>)
 for(let i = 1; i<6 ;i++){
     let userPoints = 100
+    // creating and attaching children divs(columns) to <main>
     let column =  document.createElement("div")
     $("main").append(column)
     $(column).addClass(`column${i}`)
+    // for loop creates the rows in each column
     for(let z = 0; z<6; z++){
         //inside div is a button
         let row =  document.createElement("button")
+        //if statement allows seperation of categories and quesiton
         if(z===0){
             $(row).addClass(`row${z} categories`)
             $(column).append(row)
@@ -36,6 +39,7 @@ for(let i = 1; i<6 ;i++){
             $(".column4 > .categories").html(`Slow Fashion`) 
             $(".column5 > .categories").html(`ReUse `) 
         }
+        // here lies the question buttons
         else{
             $(row).addClass(`row${z} modalLink`)
             $(column).append(row)
@@ -52,10 +56,10 @@ for(let i = 1; i<6 ;i++){
 }
 
 
-
+//create modal
 //create event listener for each box to grab the [i] object
 //[i] object includes question and answer choices
-
+// create score keeper in aside
 //  if( guess === answer){
 //                     userPoints + Number(innerText)
 //                    }else {
