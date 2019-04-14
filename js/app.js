79a3b7818ce11cf9dc980c0c710d805bc3e2d046
treeswithlove <monet.2017@gmail.com>
2019-04-14 19:46:16 -0400

    testing directing buttons

--- js/app.js
+++ js/app.js
@@ -238,7 +238,7 @@ $("footer").prepend("<div><button class='footerButtons quit'>Quit</button><butto
 
 let quitFunction = () => {
     $(".modalDisplay").css("display", "flex")
-    $(".holdQuestionInput").prepend("<div class='large'></div><a href='./content.html' class='footerButtons restart'>I'm sure</a>")
+    $(".holdQuestionInput").prepend("<div class='large'></div><a href='../gameProject/content.html' class='footerButtons restart'>I'm sure</a>")
     $(".large").html(`Are you sure you want to quit? </br>If no, select Close. If yes, select "I'm sure" `)
 }
 $(".quit").click(quitFunction)
